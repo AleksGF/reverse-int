21e0a78fa3d739a4839f3905f59bb0ff4a3f3155
AleksGF <aleksgf@mail.zp.ua>
2023-01-04 17:29:54 +0200

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const regex = new RegExp('[0-9]');
+
+  return Number(n
+      .toString()
+      .split('')
+      .filter(s => regex.test(s))
+      .reverse()
+      .join('')
+  );
 }
